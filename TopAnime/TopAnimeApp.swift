b67fc62b03f4898c5f7fb7028bb7609341f5d4ee
Art Huang <jutoart@gmail.com>
2021-12-14 23:45:40 +0800

    Initial commit of Xcode project

---
+++ TopAnime/TopAnimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopAnimeApp.swift
+//  TopAnime
+//
+//  Created by Art Huang on 2021/12/14.
+//
+
+import SwiftUI
+
+@main
+struct TopAnimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
